7297c9eb21d2a919a8a0d192901e7ced9313db48
HoangTHHE161351 <122241506+HoangTHHE161351@users.noreply.github.com>
2023-12-03 20:07:41 +0700

    create send+received document

---
+++ Project_PRN221/Project_PRN221/Pages/Documents/ReceiveDetail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project_PRN221.Pages.Documents
+{
+    public class ReceiveDetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Project_PRN221/Project_PRN221/Pages/Documents/SendDocument.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project_PRN221.Pages.Documents
+{
+    public class SendDocumentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
