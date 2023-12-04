b379ab7b9b00a9d3cc1f98b34b73a0bff5ae19db
HoangTHHE161351 <truonghoang2312@gmail.com>
2023-12-04 20:55:14 +0700

    Thêm trang nhận detail (chưa test cả trang list và detail)

--- Project_PRN221/Project_PRN221/Pages/Documents/ReceiveDetail.cshtml.cs
+++ Project_PRN221/Project_PRN221/Pages/Documents/ReceiveDetail.cshtml.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Project_PRN221.Models;
+using System.Security.Claims;
 
 namespace Project_PRN221.Pages.Documents
 {
@@ -8,8 +11,33 @@ namespace Project_PRN221.Pages.Documents
 
 	public class ReceiveDetailModel : PageModel
     {
-        public void OnGet()
+        private readonly PROJECT_SENT_DOCUMENTContext _context;
+        public ReceiveDetailModel(PROJECT_SENT_DOCUMENTContext context)
         {
+            _context = context;
+        }
+
+        public Document Document { get; set; } = default!;
+        public SendDocument SendDocument { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            if (id == null || _context.Documents == null)
+            {
+                return NotFound();
+            }
+            string email = User.FindFirstValue(ClaimTypes.Email);
+            var account = await _context.Users.SingleOrDefaultAsync(acc => acc.Email.Equals(email));
+            var sendDocument =await _context.SendDocuments.FirstOrDefaultAsync(sd=>sd.UserIdReceive == account.UserId && sd.DocumentId == id);
+            var document = await _context.Documents.Include(d=>d.User).Include(d=>d.Agence).FirstOrDefaultAsync(d=>d.DocumentId == id);
+            if (document == null || sendDocument == null)
+            {
+                return NotFound();
+            }
+            Document = document;
+            SendDocument = sendDocument;
+            return Page();
+
         }
     }
 }
