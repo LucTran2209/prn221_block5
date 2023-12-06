d1c0ffb79b2aa9229c9109a1160bd7f104c1936a
HoangTHHE161351 <truonghoang2312@gmail.com>
2023-12-06 12:22:28 +0700

    Quản lý thể loại văn bản

---
+++ Project_PRN221/Project_PRN221/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Project_PRN221.Models;
+
+namespace Project_PRN221.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+
+        private readonly PROJECT_SENT_DOCUMENTContext _context;
+        public DeleteModel(PROJECT_SENT_DOCUMENTContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                var category = await _context.Categories.SingleOrDefaultAsync(c => c.CategoryId == id);
+                if (category == null)
+                {
+                    return NotFound();
+                }
+                if(await _context.Documents.Where(d=> d.CategoryId == id).AnyAsync())
+                {
+                    ViewData["Error"] = "Có các văn bản khác thuộc thể loại " + category.CategoryName + ", không thể xóa";
+                }
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+                ViewData["Message"] = "Xóa loại văn bản thành công";
+
+            }
+            catch (Exception ex)
+            {
+                ViewData["Error"] = ex.ToString();
+            }
+            return RedirectToPage("Index");
+        }
+    }
+}
---
+++ Project_PRN221/Project_PRN221/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Project_PRN221.Models;
+
+namespace Project_PRN221.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+		private readonly PROJECT_SENT_DOCUMENTContext _context;
+		public IndexModel(PROJECT_SENT_DOCUMENTContext context)
+		{
+			_context = context;
+		}
+
+		public List<Category> Categories { get; set; } = default!;
+
+		public IActionResult OnGet()
+        {
+			GetCategories();
+			return Page();
+        }
+
+		public async Task<IActionResult> OnPostAsync(string categoryName)
+		{
+			Category category = new Category { CategoryName = categoryName};
+			try
+			{
+				_context.Categories.Add(category);
+				await _context.SaveChangesAsync();
+				ViewData["Message"] = "Thêm loại văn bản mới thành công";
+
+			}catch (Exception ex)
+			{
+				ViewData["Error"] = ex.ToString();
+			}
+			GetCategories();
+			return Page();
+		}
+
+		public void GetCategories()
+		{
+			Categories = _context.Categories.ToList();
+
+		}
+	}
+}
