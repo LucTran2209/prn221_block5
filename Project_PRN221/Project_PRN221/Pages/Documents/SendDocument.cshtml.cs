030d7a5ed6d290af10620866cd8717b316b7fdef
LucTran2209 <luctd2209@gmail.com>
2023-12-12 12:34:16 +0700

    update phan trang

--- Project_PRN221/Project_PRN221/Pages/Documents/SendDocument.cshtml.cs
+++ Project_PRN221/Project_PRN221/Pages/Documents/SendDocument.cshtml.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using NuGet.Protocol.Plugins;
 using Project_PRN221.Dto;
 using Project_PRN221.Models;
+using System.Collections.Generic;
 using System.Security.Claims;
 
 namespace Project_PRN221.Pages.Documents
@@ -17,7 +18,9 @@ namespace Project_PRN221.Pages.Documents
         {
             _context = context;
         }
-
+        public bool ShowPrevious { get; set; }
+        public bool ShowNext { get; set; }
+        public int PageIndex { get; set; }
         public string DoccumentNumber { get; set; }
         public int CategoryID { get; set; }
         public string HumanSign { get; set; }
@@ -26,40 +29,53 @@ namespace Project_PRN221.Pages.Documents
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; } = DateTime.Now;
 
-        public async Task<IActionResult> OnGetAsync(string? doccumentNumber, int? categoryId, 
-                                                    string? humanSign, DateTime? startDate, DateTime? endDate, 
-                                                    string? username, string? title)
+        public async Task<IActionResult> OnGetAsync(string? doccumentNumber, int? categoryId,
+                                                    string? humanSign, DateTime? startDate, DateTime? endDate,
+                                                    string? username, string? title,
+                                                    int pageIndex = 1)
         {
             LoadForm();
             int userId = Int32.Parse(@User.FindFirstValue("AccountId"));
-            var query = _context.SendDocuments
-                .Include(d => d.Document)
-                .Where(d => d.UserIdSend == userId)
-                .OrderBy(d => d.IsRead).ThenBy(d => d.SentDate).AsQueryable();
-            if (doccumentNumber != null) { query = query.Where(d => d.Document.DocumentNumber.ToLower().Contains(doccumentNumber.ToLower())); }
-            if (categoryId != null) { query = query.Where(d => d.Document.CategoryId == categoryId); }
-            if (humanSign != null) { query = query.Where(d => d.Document.HumanSign.ToLower().Contains(humanSign.ToLower())); }
-            if (startDate != null) { query = query.Where(d => d.SentDate >= startDate); }
-            if (endDate != null) { query = query.Where(d => d.SentDate <= endDate); }
 
-            if (title != null) { query = query.Where(d => d.Document.Title.ToLower().Contains(title.ToLower())); }
-            var documentSendDetail = from Document in _context.Documents
-                                     join sendDocument in _context.SendDocuments on Document.DocumentId equals sendDocument.DocumentId                                 
-                                     select new SendDocumentDetailDto
-                                     {
-                                         DocumentNumber = Document.DocumentNumber,
-                                         DocumentTitle = Document.Title,
-                                         DocumentDescription = Document.Description,
-                                         DocumentType = _context.Categories.SingleOrDefault(x => x.CategoryId == Document.CategoryId).CategoryName,
-                                         DocumentUrl = Document.Content,
-                                         UserReceive = _context.Users.SingleOrDefault(x => x.UserId == sendDocument.UserIdReceive).FullName,
-                                         UserSend = _context.Users.SingleOrDefault(x => x.UserId == sendDocument.UserIdSend).FullName,
-                                         AgenceReceive = _context.Users.Include(x => x.Agence).SingleOrDefault(x => x.UserId == sendDocument.UserIdReceive).Agence.AgenceName,
-                                         SentDate = sendDocument.SentDate,
-                                         IssueDate = Document.CreateDate,
-                                         HumanSign = Document.HumanSign,
-                                     };
-           // ViewData["ListDocumentSent"] = list.ToList();
+            const int pageSize = 3;
+
+            var list = from Document in _context.Documents
+                       join sendDocument in _context.SendDocuments on Document.DocumentId equals sendDocument.DocumentId
+                       where sendDocument.UserIdSend == userId
+                       select new SendDocumentDetailDto
+                       {
+                           DocumentNumber = Document.DocumentNumber,
+                           DocumentTitle = Document.Title,
+                           DocumentDescription = Document.Description,
+                           DocumentType = _context.Categories.SingleOrDefault(x => x.CategoryId == Document.CategoryId).CategoryName,
+                           DocumentUrl = Document.Content,
+                           UserReceive = _context.Users.SingleOrDefault(x => x.UserId == sendDocument.UserIdReceive).FullName,
+                           UserSend = _context.Users.SingleOrDefault(x => x.UserId == sendDocument.UserIdSend).FullName,
+                           AgenceReceive = _context.Users.Include(x => x.Agence).SingleOrDefault(x => x.UserId == sendDocument.UserIdReceive).Agence.AgenceName,
+                           SentDate = sendDocument.SentDate,
+                           IssueDate = Document.CreateDate,
+                           HumanSign = Document.HumanSign,
+                           CategoryId = _context.Categories.SingleOrDefault(x => x.CategoryId == Document.CategoryId).CategoryId,
+                           SendId = sendDocument.SendId,
+                       };
+            if (doccumentNumber != null) { list = list.Where(d => d.DocumentNumber.Contains(doccumentNumber));  DoccumentNumber = doccumentNumber; }
+            if (categoryId != null) { list = list.Where(d => d.CategoryId == categoryId); CategoryID = (int)categoryId;}
+            if (humanSign != null) { list = list.Where(d => d.HumanSign.Contains(humanSign)); HumanSign = humanSign; }
+            if (startDate != null) { list = list.Where(d => d.SentDate >= startDate); StartDate = (DateTime)startDate; }
+            if (endDate != null) { list = list.Where(d => d.SentDate <= endDate); EndDate = (DateTime)endDate; }
+            if (username != null) { list = list.Where(d => d.UserReceive.Contains(username)); Username = username; }
+            if (title != null) { list = list.Where(d => d.DocumentTitle.Contains(title)); Title = title; }
+            var totalAlbums = list.Count();
+            var totalPages = (int)Math.Ceiling((double)totalAlbums / pageSize);
+
+            // Update the ShowPrevious and ShowNext properties based on pageIndex
+            ShowPrevious = pageIndex > 1;
+            ShowNext = pageIndex < totalPages;
+
+            // Apply paging to the query
+            list = list.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+            PageIndex = pageIndex;
+            ViewData["ListDocumentSent"] = list.OrderByDescending(x => x.SentDate).ToList();
             return Page();
         }
 
@@ -68,5 +84,6 @@ namespace Project_PRN221.Pages.Documents
             ViewData["CategoryID"] = new SelectList(_context.Categories, "CategoryId", "CategoryName");
             ViewData["UserID"] = new SelectList(_context.Users, "UserId", "FullName");
         }
+       
     }
 }
